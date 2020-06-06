16d1cecf0301521865b3e173ee4c2aff832843f3
Hamza Ehtesham Farooq <41139956+EhtesHamXa@users.noreply.github.com>
2020-06-07 03:17:20 +0900

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+
+    var i = 0; 			
+var images = [];	
+var time = 3000;	
+	 
+
+images[0] = "images/slide1.jpg";
+images[1] = "images/slide2.jpg";
+images[2] = "images/slide3.jpg";
+
+
+
+function changeImg(){
+	document.slide.src = images[i];
+
+	
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	
+	setTimeout("changeImg()", time);
+}
+
+
+window.onload=changeImg;
